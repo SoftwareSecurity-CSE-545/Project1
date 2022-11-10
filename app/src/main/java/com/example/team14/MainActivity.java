4bb48848e8f40a136204d1d4dc149fc74d29cdf5
Aditya Borikar <adityaborikar2@gmail.com>
2022-11-09 20:41:19 -0700

    Refactor

--- app/src/main/java/com/example/team14/MainActivity.java
+++ app/src/main/java/com/example/team14/MainActivity.java
@@ -5,14 +5,12 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.logging.Logger;
 
-import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
@@ -21,39 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Button buttonService;
-//        Switch buttonServiceToggle;
-//
-//        buttonService = findViewById(R.id.button2);
-//        buttonServiceToggle = findViewById(R.id.switch1);
-//
-//
-//        buttonServiceToggle.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Thread myThread = null;
-//                Runnable myRunnableThread = new CountDownRunner();
-//                myThread= new Thread(myRunnableThread);
-//                if (buttonServiceToggle.isChecked())
-//                    myThread.start();
-//                else
-//                    myThread.start();
-//            }
-//        });
-//
-//        buttonService.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Thread myThread = null;
-//                Runnable myRunnableThread = new CountDownRunner();
-//                myThread= new Thread(myRunnableThread);
-//                myThread.start();
-//            }
-//        });
-//        textView = findViewById(R.id.textView5);
-//        Date currentTime = Calendar.getInstance().getTime();
-//
-//        textView.setText(currentTime.toString());
     }
 
     public void our_logger() {
@@ -73,47 +38,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ServiceActivity.class);
         startActivity(intent);
     }
-//
-//    class CountDownRunner implements Runnable{
-//        // @Override
-//        public void run() {
-//            while(!Thread.currentThread().isInterrupted()){
-//                try {
-//                    onClickServiceActivity();
-//                    Thread.sleep(1000); // Pause of 1 Second
-//                } catch (InterruptedException e) {
-//                    Thread.currentThread().interrupt();
-//                }catch(Exception e){
-//                }
-//            }
-//        }
-//    }
-//    public void onClickServiceActivity() {
-//        runOnUiThread(new Runnable() {
-//            public void run() {
-//                try{
-//                    textView = findViewById(R.id.textView5);
-//                    Date currentTime = Calendar.getInstance().getTime();
-//
-//                    Date dt = new Date();
-//                    int hours = dt.getHours();
-//                    int minutes = dt.getMinutes();
-//                    int seconds = dt.getSeconds();
-//                    String curTime = hours + ":" + minutes;
-//                    textView.setText(curTime);
-//                }catch (Exception e) {}
-//            }
-//        });
-//    }
-//    private void chaneTime() {
-//        run_on_thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                textView = findViewById(R.id.textView5);
-//                Date currentTime = Calendar.getInstance().getTime();
-//
-//                textView.setText(currentTime.toString());
-//            }
-//        })
-//    }
 }
